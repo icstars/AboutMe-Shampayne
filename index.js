dc3f9bb900e30dd3c642bf15cd4b297b04b86773
Shampaynejs <136016198+Shampaynejs@users.noreply.github.com>
2023-06-21 19:40:25 -0500

    new javascript
    new file index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+// variables
+//let, var, const
+
+// let name = "shampayne";
+
+// name = "vince";
+
+// console.log(name); // print to the console
+// //logs or prints whatever is put between " "
+// console.log("hello world");
+
+// can use "" '' `` to declare string
+
+// const name = "pizza";
+
+// console.log(name);
+
+// /* adding two strings;
+// concatenation*/
+
+// console.log(name, "pineapple");
+
+const name = "pizza";
+const topping = "pineapple";
+
+// template literals
+console.log(`${name} ${topping}`);
+
+console.log("string" + 32);
+
+console.log("32" + "100" + "str");
+
+//integer
+console.log(1 * 3);
+
+//booleans ------- false, undefined, null, 0
+let num = 1;
+
+console.log(Boolean(name));
+console.log(Boolean(num));
+
+let prompt1 = prompt("what is your name?");
+let howOldAreYou = prompt(`${prompt1}, how old are you?`);
+let whatIsYourHobby = prompt("what is your hobby?");
+// console.log(prompt1);
+
+// console.log(prompt1 + "str");
+console.log(`${prompt1} is ${howOldAreYou}, and like to ${whatIsYourHobby}`);
+
+alert(`${prompt1} is ${howOldAreYou}, and likes to ${whatIsYourHobby}`);
